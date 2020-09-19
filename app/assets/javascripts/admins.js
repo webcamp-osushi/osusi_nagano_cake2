318f9af1db52c6a0c297695d7bffdcb713a42d7c
ueda-kenta <kosian.4141@gmail.com>
2020-09-19 06:15:36 +0000

    [Fix]bootstrap

--- app/assets/javascripts/admins.js
+++ app/assets/javascripts/admins.js
@@ -1,6 +1,5 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery
-//= require popper
 //= require bootstrap-sprockets
 //= require_tree ./admins
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery
-//= require popper
 //= require bootstrap-sprockets
 //= require_tree .
